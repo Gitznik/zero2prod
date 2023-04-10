12fee578250b2c79695717a3d5e08c031d910091
Gitznik <r.offner@gmx.de>
2023-04-10 18:49:46 +0200

    Use sha3-256 for securing user passwords

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -7,6 +7,7 @@ use base64::Engine;
 use reqwest::{header, StatusCode};
 use secrecy::ExposeSecret;
 use secrecy::Secret;
+use sha3::Digest;
 use sqlx::PgPool;
 
 use crate::{domain::SubscriberEmail, email_client::EmailClient};
@@ -98,10 +99,12 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
+    let password_hash = sha3::Sha3_256::digest(credentials.password.expose_secret().as_bytes());
+    let password_hash = format!("{:x}", password_hash);
     let user_id: Option<_> = sqlx::query!(
-        r#"SELECT user_id FROM users WHERE username = $1 AND password = $2"#,
+        r#"SELECT user_id FROM users WHERE username = $1 AND password_hash = $2"#,
         credentials.username,
-        credentials.password.expose_secret()
+        password_hash,
     )
     .fetch_optional(pool)
     .await
