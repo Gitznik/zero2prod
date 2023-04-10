8df03a28c60bea6403b59a506d19a2657e34982b
Gitznik <r.offner@gmx.de>
2023-04-10 21:45:01 +0200

    Add helper for spawning blocking tasks

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -12,6 +12,7 @@ use secrecy::ExposeSecret;
 use secrecy::Secret;
 use sqlx::PgPool;
 
+use crate::telemetry::spawn_blocking_with_tracing;
 use crate::{domain::SubscriberEmail, email_client::EmailClient};
 
 use super::error_chain_fmt;
@@ -107,9 +108,8 @@ async fn validate_credentials(
         .map_err(PublishError::UnexpectedError)?
         .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))?;
 
-    let current_span = tracing::Span::current();
-    tokio::task::spawn_blocking(move || {
-        current_span.in_scope(|| verify_password_hash(expected_password_hash, credentials.password))
+    spawn_blocking_with_tracing(move || {
+        verify_password_hash(expected_password_hash, credentials.password)
     })
     .await
     .context("Failed to spawn blocking task.")
