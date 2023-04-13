61b32963d8157a46c74659dac4716e7ac2c13231
Gitznik <r.offner@gmx.de>
2023-04-13 21:25:53 +0200

    Pass error in cookies

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -12,4 +12,7 @@ async fn an_error_flash_message_is_set_on_failure() {
 
     assert_eq!(response.status().as_u16(), 303);
     assert_is_redirect_to(&response, "/login");
+
+    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
+    assert_eq!(flash_cookie.value(), "Authentication failed");
 }
