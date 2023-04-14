9bf01af327b33c770cc81493df91cb73772d3358
Gitznik <r.offner@gmx.de>
2023-04-14 16:20:06 +0200

    Remove cookie after first load

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -4,6 +4,7 @@ use crate::helpers::{assert_is_redirect_to, spawn_app};
 async fn an_error_flash_message_is_set_on_failure() {
     let app = spawn_app().await;
 
+    // Try to login
     let login_body = serde_json::json!({
         "username": "random-username",
         "password": "random-password"
@@ -13,9 +14,13 @@ async fn an_error_flash_message_is_set_on_failure() {
     assert_eq!(response.status().as_u16(), 303);
     assert_is_redirect_to(&response, "/login");
 
+    // Redirect follow
     let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
     assert_eq!(flash_cookie.value(), "Authentication failed");
-
     let html_page = app.get_login_html().await;
     assert!(html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
+
+    // Page reload
+    let html_page = app.get_login_html().await;
+    assert!(!html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
 }
