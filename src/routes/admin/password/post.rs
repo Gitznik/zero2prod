9b1e6c20440388f29bec5b29e403d85f6eec2f7e
Gitznik <r.offner@gmx.de>
2023-04-16 15:14:24 +0200

    Add missing session

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,7 +1,10 @@
 use actix_web::{web, HttpResponse};
 use secrecy::Secret;
 
-use crate::utils::{e500, see_other};
+use crate::{
+    session_state::TypedSession,
+    utils::{e500, see_other},
+};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -10,7 +13,10 @@ pub struct FormData {
     new_password_check: Secret<String>,
 }
 
-pub async fn change_password(form: web::Data<FormData>) -> Result<HttpResponse, actix_web::Error> {
+pub async fn change_password(
+    _form: web::Data<FormData>,
+    session: TypedSession,
+) -> Result<HttpResponse, actix_web::Error> {
     if session.get_user_id().map_err(e500)?.is_none() {
         return Ok(see_other("/login"));
     };
