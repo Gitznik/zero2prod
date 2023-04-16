66c176acaf200c4464da973a8b11dcb5c1f09759
Gitznik <r.offner@gmx.de>
2023-04-16 14:12:06 +0200

    Add migration for default admin user

---
+++ migrations/20230416120503_seed_user.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    '4B4ED486-5730-432A-91A8-13A05C48B60C',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$kRxbWK3Zhwkih9D50kww2Q$8YECJdX5ZdkHaW2YtGzUyes7Xlwa59qQ8dEzJhQxLU4'
+)
+
