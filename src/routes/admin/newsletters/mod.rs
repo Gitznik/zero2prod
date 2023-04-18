9415c803f63c2642276cfcb39489d9029c0e985d
Gitznik <r.offner@gmx.de>
2023-04-18 18:12:46 +0200

    Add newsletter sending landing page

---
+++ src/routes/admin/newsletters/mod.rs
@@ -0,0 +1,2 @@
+mod get;
+pub use get::submit_newsletter_issue;
--- src/startup.rs
+++ src/startup.rs
@@ -3,7 +3,7 @@ use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
 use crate::routes::{
     admin_dashboard, change_password, change_password_form, confirm, health_check, home, log_out,
-    login, login_form, publish_newsletter, subscribe,
+    login, login_form, publish_newsletter, submit_newsletter_issue, subscribe,
 };
 use actix_session::storage::RedisSessionStore;
 use actix_session::SessionMiddleware;
@@ -63,7 +63,8 @@ async fn run(
                     .route("/dashboard", web::get().to(admin_dashboard))
                     .route("/password", web::get().to(change_password_form))
                     .route("/password", web::post().to(change_password))
-                    .route("/logout", web::post().to(log_out)),
+                    .route("/logout", web::post().to(log_out))
+                    .route("/newsletters", web::get().to(submit_newsletter_issue)),
             )
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
